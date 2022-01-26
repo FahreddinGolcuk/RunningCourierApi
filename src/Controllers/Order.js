a613212dc751c92be0720d9f4ebcdcf1b4b9cb0c
Fahreddin Gölcük <fahreddinglck97@gmail.com>
2022-01-26 15:43:23 +0300

    order price has been implemented

--- src/Controllers/Order.js
+++ src/Controllers/Order.js
@@ -1,4 +1,5 @@
 import Order from '../Models/Order.js';
+import Category from '../Models/Category.js';
 
 export const createOrder = async (req, res) => {
     try {
@@ -13,6 +14,25 @@ export const createOrder = async (req, res) => {
         res.status(400).json({message: e.message});
     }
 };
+
+export const getOrderPrice = async (req,res) => {
+    const { products } = req.body
+    var price = 0
+    try {
+        const categories = await Category.find();
+        products.forEach(product => {
+            categories.forEach((category) => {
+                let finded = category['products'].find( item => item.id === product.productId )
+                if(finded) {
+                    price += (finded.price * product.quantity)
+                }
+            });
+        })
+        res.status(200).json({status: true, price: price})
+    } catch(e) {
+        res.status(400).json({message: e.message, status: false})
+    }
+}
 
 export const getOrderHistory = async (req, res) => {
     try {
--- src/Routers/Order.js
+++ src/Routers/Order.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import {createOrder, getOrderHistory} from '../Controllers/Order.js'
+import {createOrder, getOrderHistory, getOrderPrice} from '../Controllers/Order.js'
 import {authenticateJWT} from "../Core/Auth.js";
 const router = express.Router();
 
 router.post('/create-order', authenticateJWT, createOrder);
 router.get('/order-history', authenticateJWT, getOrderHistory);
+router.post('/cart-price', getOrderPrice)
+
 export default router;
